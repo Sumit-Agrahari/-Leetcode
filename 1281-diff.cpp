89096a58cf638443385dd544547bad2cd1f223d0
Sumit Kumar Agrahari <83569086+Sumit-Agrahari@users.noreply.github.com>
2022-08-19 14:34:51 +0530

    Add files via upload

---
+++ 1281-diff.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int Diff(int n) {
+        int prod=1,sum=0;
+        while(n!=0){
+            int digit=n%10;
+            prod=prod*digit;
+            sum=sum+digit;
+            n=n/10;
+        }
+        int ans=prod-sum;
+        return ans;
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<Diff(n);
+}
